8e8118dbe274432dd12c6407bfa20baf002084da
yokotakohei <47553884+yokotakohei@users.noreply.github.com>
2025-12-30 16:36:49 +0900

    Add files via upload
    Add a key map of NvimTreeToggle.

--- init.lua
+++ init.lua
@@ -78,6 +78,11 @@ require("config.ripgrep_config").setup()
 vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+-- Telescope で参照を一覧します。
+vim.keymap.set("n", "gR", function()
+    require("telescope.builtin").lsp_references()
+end)
+
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
@@ -122,6 +127,9 @@ vim.keymap.set("n", "<F4>", ":w<CR>:cd %:h<CR>:!gfortran % -o %:r<CR>")
 
 -- "<現在開いているファイル名>.exe" を実行します。
 vim.keymap.set("n", "<S-F4>", ":cd %:h<CR>:!start cmd /K %:r.exe&timeout -1&exit + {enter}<CR>")
+
+-- NvimTree を開閉します。
+vim.keymap.set("n", "<F12>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 --------------------------------------------------------------------------------
 -- ファイル読み込み設定
@@ -357,3 +365,43 @@ vim.api.nvim_create_user_command('CopilotChatOpenHistoryDir', function()
   local history_path = vim.fn.expand("~/.local/share/nvim/copilot_chat_history")
   vim.cmd("edit " .. history_path)
 end, { desc = "CopilotChat 履歴ディレクトリを開く" })
+
+
+--------------------------------------------------------------------------------
+-- IME の切替
+--------------------------------------------------------------------------------
+
+-- 実行環境を判定する関数
+local function detect_environment()
+  local env = {}
+
+  -- Linux
+  env.is_linux = vim.fn.has("unix") == 1 and not vim.fn.has("wsl") == 1
+
+  -- Windows
+  env.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+
+  -- WSL
+  env.is_wsl = vim.fn.has("wsl") == 1
+
+  -- VSCode Neovim
+  env.is_vscode = vim.g.vscode ~= nil
+
+  return env
+end
+
+-- 実行環境を取得
+local env = detect_environment()
+
+-- モード変更時IME自動オフ
+if env.is_linux then
+  vim.api.nvim_set_keymap("i", "<silent> <Esc>", "<Esc>:call system('fcitx5-remote -c')<CR>", { noremap = true })
+elseif env.is_vscode or env.is_wsl then
+  vim.o.shell = "/usr/bin/bash --login"
+  vim.cmd('autocmd InsertLeave * :call system("zenhan.exe 0")')
+  vim.cmd('autocmd CmdlineLeave * :call system("zenhan.exe 0")')
+elseif env.is_windows then
+  vim.o.shell = "cmd.exe"
+  vim.cmd("autocmd InsertLeave * :call system('zenhan 0')")
+  vim.cmd("autocmd CmdlineLeave * :call system('zenhan 0')")
+end
