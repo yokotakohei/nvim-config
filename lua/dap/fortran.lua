fedbdcac9a7eae84da1f7c8a7374ef85227578e0
yokotakohei <47553884+yokotakohei@users.noreply.github.com>
2026-01-21 21:41:34 +0900

    Change Fortran DAP

--- lua/dap/fortran.lua
+++ lua/dap/fortran.lua
@@ -3,37 +3,42 @@ local M = {}
 function M.setup()
   local dap = require("dap")
 
-  -- Fortran デバッガアダプターの設定 (codelldb または gdb を使用)
-  -- codelldb を優先的に使用 (MasonInstall でインストール可能)
-  if not dap.adapters.codelldb then
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = "${port}",
-      executable = {
-        command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
-        args = {"--port", "${port}"},
-      }
+  ---------------------------------------------------------------------------
+  -- Debug adapters
+  ---------------------------------------------------------------------------
+
+  -- gdb adapter (DAP interpreter)
+  if not dap.adapters.gdb then
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "gdb",
+      args = { "--interpreter=dap" },
     }
   end
 
-  -- Fortran デバッグ構成
+  ---------------------------------------------------------------------------
+  -- Fortran debug configurations
+  ---------------------------------------------------------------------------
+
   dap.configurations.fortran = {
     {
-      name = "Launch file",
-      type = "codelldb",
+      name = "Launch file (gdb)",
+      type = "gdb",
       request = "launch",
+
+      -- 実行ファイルを対話的に指定する。
       program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        return vim.fn.input(
+          "Path to executable: ",
+          vim.fn.getcwd() .. "/",
+          "file"
+        )
       end,
-      cwd = '${workspaceFolder}',
-      stopOnEntry = false,
-    },
-    {
-      name = 'Attach to process',
-      type = 'codelldb',
-      request = 'attach',
-      pid = require('dap.utils').pick_process,
-      args = {},
+
+      cwd = "${workspaceFolder}",
+
+      -- main program の先頭で停止する。
+      stopAtBeginningOfMainSubprogram = true,
     },
   }
 end
