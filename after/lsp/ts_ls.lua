4581293ad0cbe898e6835b1796384616262117dc
yokota.kohei <yokota.kohei.jpn@gmail.com>
2026-03-02 09:59:56 +0900

    tsserver に関するコメントを追加

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -1,5 +1,6 @@
 ---@type vim.lsp.Config
 return {
+    -- npm install -g typescript で tsserver をインストールすること．
     cmd = { "typescript-language-server", "--stdio" },
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     autostart = true,
